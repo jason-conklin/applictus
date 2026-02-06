f0b74d74ec7e05f726630ccd4bb3de626b5f2394
jason-conklin <jc2353@njit.edu>
2026-02-05 19:49:47 -0500

    Transitioning from Render to Vercel 2

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,9 @@
 const { app } = require('../server/src/app');
 
-module.exports = app;
+module.exports = (req, res) => {
+  const url = req.url || '';
+  if (!url.startsWith('/api/')) {
+    req.url = `/api${url.startsWith('/') ? url : `/${url}`}`;
+  }
+  return app(req, res);
+};
